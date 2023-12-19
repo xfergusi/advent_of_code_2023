da832de26babd2fd7bdc98a83c0f035f5004e700
Ian Ferguson <ian@Ian-Fergusons-MacBook-Air.local>
2023-12-19 11:23:45 +1100

    day2 done

--- day2/day2.py
+++ day2/day2.py
@@ -1,7 +1,12 @@
 f = open("input", "r")
 answer = 0
 for line in f:
-    possible = True
+    dic = {
+        "red": 0,
+        "green": 0,
+        "blue": 0
+    }
+    print("---")
     print(line)
     split1 = line.split(":")
     game_str_num = split1[0]
@@ -10,13 +15,18 @@ for line in f:
     for round in rounds:
         pulls_info = round.split(",")
         for pull_info in pulls_info:
-            num_part = pull_info.trim().split(" ")[0]
-            color_part = pull_info.trim().split(" ")[1]
-            if color_part == "red" and num_part > 12:
-                possible = False
-            if color_part == "green" and num_part > 13:
-                possible = False
-            if color_part == "blue" and num_part > 14:
-                possible = False
-    if possible:
-        answer += int(game_str_num.split(" ")[1])
+            num_part = int(pull_info.strip().split(" ")[0])
+            color_part = pull_info.strip().split(" ")[1]
+            print(pull_info)
+            if color_part == "red" and num_part > dic["red"]:
+                dic["red"] = num_part
+            if color_part == "green" and num_part > dic["green"]:
+                dic["green"] = num_part
+            if color_part == "blue" and num_part > dic["blue"]:
+                dic["blue"] = num_part
+    game_math = 1
+    for key in dic:
+        game_math = game_math*dic[key]
+            
+    answer += game_math
+print(answer)
